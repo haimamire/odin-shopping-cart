6f8a2a260ad41e9e336ab27454ed996c39516de6
haimamire <ivanmatusevich16@gmail.com>
2026-09-17 19:53:00 -0300

    feat(cart): implement localStorage

--- src/hooks/use-shopping-cart.jsx
+++ src/hooks/use-shopping-cart.jsx
@@ -1,8 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const useShoppingCart = () => {
-  const [cartProducts, setCartProducts] = useState([]);
+  const [cartProducts, setCartProducts] = useState(
+    JSON.parse(localStorage.getItem("cartProducts")) ?? [],
+  );
 
+  useEffect(() => {
+    localStorage.setItem("cartProducts", JSON.stringify(cartProducts));
+  }, [cartProducts]);
+
+  // Functions that update the shopping cart
   const addCartProduct = (newProduct) => {
     const newCart = [...cartProducts];
 
